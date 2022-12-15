6b98024164876bd8abab588e06e276b165bd06ca
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2022-12-15 22:18:02 +0300

    CREATE SPRING PROJECT

---
+++ src/main/java/com/example/billtracking/BilltrackingApplication.java
@@ -0,0 +1,12 @@
+package com.example.billtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BilltrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilltrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/billtracking/BilltrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.billtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilltrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
