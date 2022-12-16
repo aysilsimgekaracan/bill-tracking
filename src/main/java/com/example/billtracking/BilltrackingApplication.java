c3f0f95c6e22a366906a03dd1206e708c3611be0
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2022-12-16 12:40:07 +0300

    16_12_2022

--- src/main/java/com/example/billtracking/BilltrackingApplication.java
+++ src/main/java/com/example/billtracking/BilltrackingApplication.java
@@ -1,12 +1,42 @@
 package com.example.billtracking;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.CommandLineRunner;
+
 @SpringBootApplication
-public class BilltrackingApplication {
+@RestController
+public class BilltrackingApplication implements CommandLineRunner {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+/*
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+*/
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO User (name, surname, username, email, password) VALUES (?, ?, ?, ?, ?)";
+		int result = jdbcTemplate.update(sql, "aysil", "karacan", "username3", "email@email.com", "12345");
+
+		if (result > 0) {
+			System.out.println("A new row has been inserted.");
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BilltrackingApplication.class, args);
+
 	}
+
 
 }
